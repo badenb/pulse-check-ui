c66218e77b00d9038d75ae1001fcf11195cf04f2
Baden <baden1998@gmail.com>
2025-05-19 11:06:12 -0500

    Initial Commit

---
+++ pulse-check-ui/pulse_check_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_check_uiApp.swift
+//  pulse-check-ui
+//
+//  Created by Baden Bennett on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct pulse_check_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pulse-check-uiTests/pulse_check_uiTests.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_check_uiTests.swift
+//  pulse-check-uiTests
+//
+//  Created by Baden Bennett on 5/19/25.
+//
+
+import Testing
+@testable import pulse_check_ui
+
+struct pulse_check_uiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
